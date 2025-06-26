df0c277db4f1e6b9f1311bc692548a3c6f4008cc
Gabriel Antonius <gabriel.antonius@gmail.com>
2025-06-26 15:08:46 -0400

    CLI parser overhaul

--- myfiles/interface/cli_functions.py
+++ myfiles/interface/cli_functions.py
@@ -1,101 +1,198 @@
+from abc import abstractmethod, ABC
+from argparse import ArgumentParser, Namespace
+
+
+def get_cli_functions():
+    """Return a list of callable functions."""
+    functions = []
+    for fclass in _get_all_subclasses(CLIfunction):
+        try:
+            func = fclass()
+            name = fclass.__name__
+            functions.append((name, func))
+        except TypeError:
+            continue
+    functions = sorted(functions, key=lambda f: f[0])
+    names, funcs = zip(*functions)
+    return funcs
+
+def _get_all_subclasses(cls):
+    """Recursively retrieve all subclasses of a given class."""
+    all_subclasses = set()
+    for subclass in cls.__subclasses__():
+        all_subclasses.add(subclass)
+        all_subclasses.update(_get_all_subclasses(subclass))
+    return list(all_subclasses)
+
+# =========================================================================== #
+# Base classes
+# =========================================================================== #
+
+class CLIfunction(ABC):
+    """Base class for functions with command-line interface."""
+
+    @abstractmethod
+    def __call__(self, args: Namespace):
+        pass
+
+    def add_parser(self, subparsers) -> ArgumentParser:
+        parser = subparsers.add_parser(self.__class__.__name__,
+                                       help=self.__class__.__doc__)
+        parser.set_defaults(func=self)
+        return parser
+
+
+class CLIfunctionWithID(CLIfunction):
+    """Base class for functions that operate on a node ID."""
+
+    def add_parser(self, sub):
+        parser = super().add_parser(sub)
+        parser.add_argument('ids', type=int, nargs='+', help='Node ID.')
+        return parser
+
+
+class CLIfunctionWithRemote(CLIfunction):
+    """Base class for functions that operate on a remote host."""
+
+    def add_parser(self, sub):
+        parser = super().add_parser(sub)
+        parser.add_argument('hostname', type=str, help='Remote host.')
+        return parser
+
+# =========================================================================== #
+# Functions
+# =========================================================================== #
+
 from .. import UserConfig, ProjectConfig, RemoteHosts
 from .. import Node, Project
 
-
-# =========================================================================== #
-# Configurating and inspecting
 # =========================================================================== #
 
-def check_config():
+class check_config(CLIfunction):
+    """Print out configuration."""
+    def __call__(self, args):
 
-    userconfig = UserConfig()
-    if not userconfig.file_exists:
-        userconfig.write_config_file()
+        userconfig = UserConfig()
+        if not userconfig.file_exists:
+            userconfig.write_config_file()
 
-    try:
-        projconfig = ProjectConfig()
-        print(projconfig)
+        try:
+            projconfig = ProjectConfig()
+            print(projconfig)
 
-        if not projconfig.file_exists:
-            if projconfig.topdir.exists():
-                projconfig.write_config_file()
-    except:
-        pass
+            if not projconfig.file_exists:
+                if projconfig.topdir.exists():
+                    projconfig.write_config_file()
+        except:
+            pass
 
-    rh = RemoteHosts()
-    print(rh)
+        rh = RemoteHosts()
+        print(rh)
 
+class check_remote(CLIfunction):
+    """Print out remote hosts."""
+    def __call__(self, args):
+        rh = RemoteHosts()
+        print(rh)
 
-def check_user_config():
-    rc = UserConfig()
-    print(rc)
-    if not rc.file_exists:
-        rc.write_config_file()
+class check_project(CLIfunction):
+    """Display project information."""
+    def __call__(self, args):
+        proj = Project()
+        print(proj)
+    
+class check_user_config(CLIfunction):
+    """Print out user configuration."""
+    def __call__(self, args):
+        rc = UserConfig()
+        print(rc)
+        if not rc.file_exists:
+            rc.write_config_file()
 
-def check_project_config():
-    rc = ProjectConfig()
-    print(rc)
-    if not rc.file_exists:
-        rc.write_config_file()
+class check_project_config(CLIfunction):
+    """Print out project configuration."""
+    def __call__(self, args):
+        rc = ProjectConfig()
+        print(rc)
+        if not rc.file_exists:
+            rc.write_config_file()
 
-def check_remote():
-    rh = RemoteHosts()
-    print(rh)
+class check_node(CLIfunctionWithID):
+    """Print out information about a node."""
 
-def check_node(ids=None):
-    node = Node(ids=ids)
-    if node:
-        node.scan()
-        print(node)
-    else:
-        print(node.ids)
+    def __call__(self, args):
+        node = Node(ids=args.ids)
+        if node:
+            node.scan()
+            print(node)
+        else:
+            print(node.ids)
 
-def check_project():
-    proj = Project()
-    print(proj)
+class newproj(CLIfunction):
+    """
+    Create a new project, with directories and configuration files.
+    Will not overwrite an existing project.
+    """
+    def add_parser(self, sub):
+        parser = super().add_parser(sub)
+        parser.add_argument('name', type=str, help='Project name')
+        return parser
 
-def newproj(name):
-    return Project.new_project(name)
+    def __call__(self, args):
+        return Project.new_project(args.name)
 
 # =========================================================================== #
-# Syncing data
-# =========================================================================== #
 
-def pull_remote(hostname, ids):
-    proj = Project()
-    proj.pull_production_dir(hostname, ids)
+class pull_remote(CLIfunctionWithRemote, CLIfunctionWithID):
+    """Pull files from a remote host."""
+    def __call__(self, args):
+        proj = Project()
+        proj.pull_production_dir(args.hostname, args.ids)
 
-def push_remote(hostname, ids):
-    proj = Project()
-    proj.push_production_dir(hostname, ids)
+class push_remote(CLIfunctionWithRemote, CLIfunctionWithID):
+    """Push files to a remote host."""
+    def __call__(self, args):
+        proj = Project()
+        proj.push_production_dir(args.hostname, args.ids)
 
-def pull_scratch(ids):
-    proj = Project()
-    proj.pull_scratch(ids)
+class pull_scratch(CLIfunctionWithID):
+    """Pull files from scratch directory."""
+    def __call__(self, args):
+        proj = Project()
+        proj.pull_scratch(args.ids)
 
-def push_scratch(ids):
-    proj = Project()
-    proj.push_scratch(ids)
+class push_scratch(CLIfunctionWithID):
+    """Push files to scratch directory."""
+    def __call__(self, args):
+        proj = Project()
+        proj.push_scratch(args.ids)
 
-def push_local_data(hostname):
-    proj = Project()
-    proj.push_local_data(hostname)
+class push_local_data(CLIfunctionWithRemote):
+    """Push local data to a remote host."""
+    def __call__(self, args):
+        proj = Project()
+        proj.push_production_dir(args.hostname, args.ids)
+        proj.push_local_data(args.hostname)
 
-def push_glocal_data(hostname):
-    proj = Project()
-    proj.push_global_data(hostname)
-
+class push_global_data(CLIfunctionWithRemote):
+    """Push global data to a remote host."""
+    def __call__(self, args):
+        proj = Project()
+        proj.push_global_data(args.hostname)
 
 # =========================================================================== #
 # Moving data
 # =========================================================================== #
 
+class make_anadir(CLIfunctionWithID):
+    """Create a directory in analysis folder to match one in the production folder."""
+    def __call__(self, args):
+        node = Node(ids)
+        node.make_analysis_dir(verbose=True)
 
-def make_anadir(ids):
-    node = Node(ids)
-    node.make_analysis_dir(verbose=True)
 
-def save_results():
-    project = Project()
-    project.copy_analysis_files_to_results()
-    pass
+class save_results(CLIfunction):
+    """Create a directory in analysis folder to match one in the production folder."""
+    def __call__(self, args):
+        project = Project()
+        project.copy_analysis_files_to_results()
--- myfiles/interface/command_line.py
+++ myfiles/interface/command_line.py
@@ -1,84 +1,26 @@
 from textwrap import dedent
 from argparse import ArgumentParser
+from .cli_functions import get_cli_functions
 
-from . import cli_functions
+__all__ = ['command_line_execution']
 
 def command_line_execution():
 
     parser = ArgumentParser(
         prog='myfiles', 
-        description = dedent(
-        """\
-        Project manager
-        """),   
-        #epilog = ("""Blob"""),
+        description = 'Project management.',
         )   
     
-    parser.add_argument(
-        'command', metavar='command',
-        help= (
-        """\
-        Execute one of these commands:
-            check_config
-            check_project
-            check_node
-            show_nodes
-            show_remote
-            push_remote
-            pull_remote
-            push_scratch
-            pull_scratch
-            push_global_data
-            push_local_data
-            make_anadir
-            save_results
-            newproj
-        """),
-        default='check_project',
-        #nargs = '*',
-        choices = ('check_config', 'check_project', 'check_node',   
-                   'check_nodes',
-                   'check_remote',
-                   'push_remote', 'pull_remote',
-                   'push_scratch', 'pull_scratch',
-                   'push_global_data', 'push_local_data',
-                   'make_anadir',
-                   'save_results',
-                   'newproj',
-            ),
-        )
+    #parser.add_argument(
+    #    '-d', '--dry-run', action='store_true',
+    #    dest='dry_run',
+    #    help="""Print command but don't do it.""",
+    #    )
+
+    subparsers = parser.add_subparsers(title='command', required=True)
     
-    parser.add_argument(
-        '-d', '--dry-run', action='store_true',
-        dest='dry_run',
-        help="""Print command but don't do it.""",
-        )
+    for func in get_cli_functions():
+        func.add_parser(subparsers)
 
-    # A very wacky way to do it...
-    #args = parser.parse_args()
     args, other_args = parser.parse_known_args()
-    func_name = args.command
-
-    if func_name not in dir(cli_functions):
-        print('Command not found: {func_name}')
-        return
-
-    func = getattr(cli_functions, func_name)
-    result = func(*other_args)
-
-    #g = globals()
-    #if func_name in g:
-    #    g[func_name](*other_args)
-    #else:
-    #    print('Command not found: {func_name}')
-    #    print(g)
-
-    #match func_name:
-    #    case 'check_config':    
-    #        check_config()
-    #    case 'check_project':    
-    #        check_project()
-    #    case 'check_node':    
-    #        check_node()
-    #    case 'check_remote':    
-    #        check_remote()
+    args.func(args)
